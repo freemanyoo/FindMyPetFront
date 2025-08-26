d8d9c9c627163f13d39cb436bc36cbe5cdc05da7
freemanyoo <freemanyoo@gmail.com>
2025-08-26 10:33:57 +0900

    폴더정리 및 회원가입및 로그인 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from "./layouts/MainLayout";
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
 
 import './App.css';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        {/* Placeholder routes for now */}
-        <Route path="/board/missing" element={<div>Missing Board Page</div>} />
-        <Route path="/board/shelter" element={<div>Shelter Board Page</div>} />
-        <Route path="/login" element={<div>Login Page</div>} />
-        <Route path="/register" element={<div>Register Page</div>} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="login" element={<LoginPage />} />
+                    <Route path="register" element={<RegisterPage />} />
+                    {/* Keeping other placeholders for now */}
+                    <Route path="board/missing" element={<div>Missing Board Page</div>} />
+                    <Route path="board/shelter" element={<div>Shelter Board Page</div>} />
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
