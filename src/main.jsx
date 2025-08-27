1daf0414a6ab6829085f9988e2d35fe6420f67e2
coko9437 <coko9437@gmail.com>
2025-08-28 07:44:13 +0900

    post2

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+ // bootstrap CSS를 프로젝트에 적용
+
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <AuthProvider>
